290994b0b95bf991440d448265b43c37f0e018d9
Szymon Lewandowski <szymonlewandowski575@gmail.com>
2023-03-29 23:41:25 +0200

    feat: read email body

--- src/main/java/com/example/emailsecurityassessment/message/Message.java
+++ src/main/java/com/example/emailsecurityassessment/message/Message.java
@@ -1,4 +1,12 @@
 package com.example.emailsecurityassessment.message;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Message {
+    @Id
+    private Long id;
+    private String sender;
+    private String recipient;
 }
--- src/main/java/com/example/emailsecurityassessment/message/MessageRepository.java
+++ src/main/java/com/example/emailsecurityassessment/message/MessageRepository.java
@@ -1,4 +1,8 @@
 package com.example.emailsecurityassessment.message;
 
-public interface MessageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
